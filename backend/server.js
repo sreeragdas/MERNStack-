89f1897ae2b13081d4aecc4532a570d101374bd5
EmbarckleSreerag <sreerag.em@embarckle.com>
2022-12-06 14:00:33 +0530

    git commit:file changed and code updated

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,24 @@ const mongoose = require('mongoose')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app=express();
-const port = process.env.PORT || 5000;
+const port =  5000;
 app.use(express.json());
-const uri="mongodb+srv://sreerag:vaishakh@cluster0.xqc4l.mongodb.net/?retryWrites=true&w=majority"
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
- console.log("connection established")
-});
+const uri="mongodb+srv://sreerag:vaishakh@cluster0.xqc4l.mongodb.net/?retryWrites=true&w=majority&ssl=true"
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+    );
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+      console.log("MongoDB database connection established successfully");
+    })
 
 app.get('/',(req,res)=>{
     res.send('goodday!')
 })
 const exerciseRouter=require('./routes/exercise')
 const userRouter=require('./routes/user')
-app.use('./exercise',exerciseRouter)
-app.use('./user',userRouter)
+app.use('/exercise',exerciseRouter)
+app.use('/users',userRouter)
 app.listen(port , ()=>{
     console.log(`Server is running in port : ${port}`)
 })
